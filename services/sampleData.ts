5cd1df91f07ba167e552d059a1151d6171ce1ed0
Permia Community <35648833+sakaturu@users.noreply.github.com>
2026-02-08 21:25:25 -0600

    Update sampleData.ts

--- services/sampleData.ts
+++ services/sampleData.ts
@@ -41,7 +41,7 @@ const INITIAL_VIDEO_DATA: any[] = [
   },
   {
     "id": "v-fav-001",
-    "prompt": "Heal The Earth555",
+    "prompt": "Heal The Earth999",
     "category": "Fav. Pick",
     "url": "_5aA54MD4ho",
     "thumbnail": "https://img.youtube.com/vi/_5aA54MD4ho/mqdefault.jpg"
